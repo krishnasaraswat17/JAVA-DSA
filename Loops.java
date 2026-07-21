c2d2570cabe72e5df43024ce27d165b17010eb2b
krishna saraswat <krishnasaraswat1712@gmail.com>
2026-07-21 10:22:25 +0530

    UPDATED CODE ( IT INCLUDES WHILE,FOR LOOP AND SOME SYNTAX  Practice Questions )

---
+++ Loops.java
@@ -0,0 +1,137 @@
+
+public class Loops {
+    public static void main(String[] args) {
+
+        //While loop
+
+    //     int count = 1;
+    //     while(count !=5){
+    //         System.out.println(count);
+    //         count++;
+    // }
+
+    // for loop
+    //for(int count=1;count!=10;count++){
+       // System.out.println(count);
+    //}
+//     System.out.println("Enter the Temp");
+//     Scanner input = new Scanner(System.in);
+
+//     float c = input.nextFloat();
+
+//     float f = (c*(9f/5f))+32;
+
+//     System.out.println(f);
+//     input.close();
+
+// }
+
+
+
+
+//  PRACTICE QUESTIONS 
+
+// Write Java programs for the following:
+// Write a program to print whether a number is even or odd, also take input from the user.
+// Take name as input and print a greeting message for that particular name.
+// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+// Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+// To calculate Fibonacci Series up to n numbers.
+// To find out whether the given String is Palindrome or not.
+// To find Armstrong Number between two given number.
+
+
+// 1 Write a program to print whether a number is even or odd, also take input from the user.
+
+    // System.out.println("ENTER A NUMBER ");
+    // Scanner input = new Scanner(System.in);
+
+    // int number = input.nextInt();
+
+    // if(number %2 ==0){
+    //     System.out.println("THIS IS A EVEN NUMBER ");
+    // }
+    // else{
+    //     System.out.println("THIS IS A ODD NUMBER");
+    // }
+    // input.close();
+
+
+// 2  Take name as input and print a greeting message for that particular name.
+    
+    // System.out.println("ENTER THE NAME");
+    // Scanner input = new Scanner(System.in);
+
+    // String name = input.nextLine();
+    // System.out.println("RAM RAM JI " +name);
+    // input.close();
+    
+
+    // 2 Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+    // System.out.println("ENTER PRINCIPAL");
+
+    // Scanner input = new Scanner(System.in);
+    // float Principal = input.nextFloat();
+
+    // System.out.println("ENTER RATE");
+    // float Rate = input.nextFloat();
+
+    // System.out.println("ENTER TIME");
+    // float Time = input.nextFloat();
+
+    // float SimpleInterest = (Principal*Rate*Time)/100;
+
+    // System.out.println("SIMPLE INTEREST = " + SimpleInterest);
+
+    // input.close();
+    
+    // 3 Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+    // System.out.println("ENTER FIRST NUMBER");
+    // Scanner input = new Scanner(System.in);
+
+    // float FirstNumber = input.nextFloat();
+
+    // System.out.println("ENTER SECOND NUMBER");
+
+    // float SecondNumber = input.nextFloat();
+
+    // System.out.println("SELECT OPERATOR (+,-,%,*,/)");
+
+    // String Operator = input.next();
+
+    // if(Operator.equals("+")){
+    //     float sum = FirstNumber + SecondNumber;
+    //     System.out.println("FirstNumber + SecondNumber = " + sum );
+    // }
+
+    // else if(Operator.equals("-")){
+    //     float sub = FirstNumber - SecondNumber;
+    //     System.out.println("FirstNumber - SecondNumber = " + sub);
+    // }
+
+    // else if(Operator.equals("%")){
+    //     float mod = FirstNumber % SecondNumber;
+    //     System.out.println("FirstNumber % SecondNumber = " + mod);
+    // }
+
+    // else if(Operator.equals("*")){
+    //     float mul = FirstNumber * SecondNumber;
+    //     System.out.println("FirstNumber * SecondNumber = " + mul);
+    // }
+
+    // else if (Operator.equals("/")) {
+    // if (SecondNumber != 0) {
+    //     float div = FirstNumber / SecondNumber;
+    //     System.out.println("Answer = " + div);
+    // } else {
+    //     System.out.println("Cannot divide by zero.");
+    // }
+    // }
+
+    // else{
+    //     System.out.println("WRONG OPERATOR SELECTED");
+    // }
+    // input.close();
+}
+}
