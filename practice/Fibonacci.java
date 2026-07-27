977dacd8cba23bedf294020246a87d56106071c8
krishna saraswat <krishnasaraswat1712@gmail.com>
2026-07-27 12:01:29 +0530

    Fibonacci Number Code

---
+++ practice/Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Fibonacci{
+    public static void main(String[] agrs){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER A NUMBER :");
+
+        int n = input.nextInt();
+
+        int a = 0;
+
+        int b = 1;
+
+        int count = 2;
+ 
+        while(count <= n){
+            int temp = b ;
+            b = b + a ;
+            a = temp ;
+            count++;        
+        }
+        System.out.println(b);
+    }
+}
