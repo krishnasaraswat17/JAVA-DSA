045a1878e2c2c05cbb75e9e96bb0d2627d829493
krishna saraswat <krishnasaraswat1712@gmail.com>
2026-08-03 22:10:44 +0530

    how to use function in java example

---
+++ function.java
@@ -0,0 +1,9 @@
+public class function {
+    public static void main(String[] args) {
+        sum(10, 20);
+    }
+
+    private static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+}
